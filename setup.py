3a4246696f37495a534ba9aaf8d1d39d56b26459
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2018-12-03 11:09:45 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+
+
+setup(name='markingpy',
+      author='InAKleinBottle',
+      email='admin@inakleinbottle.com',
+      version='1.0.0',
+      packages=['markingpy'],
+      install_requires=['pylint'],
+      test_suite='tests'
+      )
