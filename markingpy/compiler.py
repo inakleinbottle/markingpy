c954adc1976d62227b122a41895f7abb23530b7a
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2019-03-19 13:30:01 +0000

    Started implementing new abstract syntax checker interface.

--- markingpy/compiler.py
+++ markingpy/compiler.py
@@ -1,5 +1,5 @@
 #      Markingpy automatic grading tool for Python code.
-#      Copyright (C) 2019 Sam Morley
+#      Copyright (C) 2019 University of East Anglia
 #
 #      This program is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU General Public License as published by
